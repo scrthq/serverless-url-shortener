ed347d4d77e339f8557107c95bf451dcf1cbf1bd
Nate Ferrell <nate@scrthq.com>
2019-08-12 12:35:04 -0500

    added cleanup and working code

--- scripts/build-template.js
+++ scripts/build-template.js
@@ -9,9 +9,12 @@ try {
   let readPath = path.resolve(__dirname, '../static/template.html')
   let template = fs.readFileSync(readPath).toString()
   template = template.replace('INSERT_API_URL', config['API_URL'])
+  template = template.replace('INSERT_REPO_URL', config['REPO_URL'])
+  template = template.replace('INSERT_PAGE_TITLE', config['PAGE_TITLE'])
+  template = template.replace('INSERT_PAGE_TITLE', config['PAGE_TITLE']) // twice since it needs to replace both instances
 
   let writePath = path.resolve(__dirname, '../static/index.html')
-  fs.writeFileSync(writePath, template)  
+  fs.writeFileSync(writePath, template)
 
   console.log('Template build success')
 
